1026edd0e36067421b775abd28fd256b738416d1
raresgaia123 <137071040+raresgaia123@users.noreply.github.com>
2025-01-29 20:56:30 +0200

    feat: update job config with the deploy flag (#92)
    Based on the deployment policy, the workers are split between available agents.
    Therefore, each agent will receive the full config, but each worker that needs to be deployed by that agent will have a new flag.
    Implemented config update based on worker distribution returned by the policy.

--- infscale/controller/deployment/policy.py
+++ infscale/controller/deployment/policy.py
@@ -16,22 +16,36 @@ class DeploymentPolicy(ABC):
     """Abstract class for deployment policy."""
 
     @abstractmethod
-    def split(self, job_config: JobConfig):
-        """Split the job config using deployment policy."""
-        # Placeholder method for splitting job config
+    def split(self, job_config: JobConfig) -> dict[str, JobConfig]:
+        """
+            Split the job config using random deployment policy
+            and return updated job config for each agent.
+        """
         pass
 
+    def _get_agent_updated_cfg(self, wrk_distr: dict[str, list[str]], job_config: JobConfig) -> dict[str, JobConfig]:
+        """Return updated job config for each agent."""
+        agents_config = {}
+        for agent_id, wrk_ids in wrk_distr.items():
+            for w in job_config.workers:
+                # set the deploy flag if the worker is in worker distribution for this agent
+                w.deploy = w.id in wrk_ids
+
+            agents_config[agent_id] = job_config
+
+        return agents_config
 
 class EvenDeploymentPolicy(DeploymentPolicy):
     """Even deployment policy class."""
 
-    def split(self, agent_ids: list[str], job_config: JobConfig) -> dict[str, str]:
+    def split(self, agent_ids: list[str], job_config: JobConfig) -> dict[str, JobConfig]:
         """
-        Split the job config using even deployment policy.
+        Split the job config using even deployment policy
+        and return updated job config for each agent.
 
         Workers are distributed as evenly as possible across the available agents.
         If the number of workers isn't perfectly divisible by the number of agents,
-        the "extra" workers are assigned to the first agents in the list
+        the "extra" workers are assigned to the first agents in the list.
         """
         # dictionary to hold the workers for each agent_id
         distribution = {agent_id: [] for agent_id in agent_ids}
@@ -50,31 +64,32 @@ class EvenDeploymentPolicy(DeploymentPolicy):
                 1 if i < remaining_workers else 0
             )
 
-            # assign only worker["id"] to the current agent
+            # assign only worker id to the current agent
             distribution[agent_id] = [
-                worker["id"]
+                worker.id
                 for worker in workers[start_index : start_index + num_workers_for_agent]
             ]
 
             # move the start index to the next batch of workers
             start_index += num_workers_for_agent
 
-        return distribution
+        return self._get_agent_updated_cfg(distribution, job_config)
 
 
 class RandomDeploymentPolicy(DeploymentPolicy):
     """Random deployment policy class."""
 
-    def split(self, agent_ids: list[str], job_config: JobConfig):
+    def split(self, agent_ids: list[str], job_config: JobConfig) -> dict[str, JobConfig]:
         """
-        Split the job config using random deployment policy.
+        Split the job config using random deployment policy
+        and return updated job config for each agent.
 
         Each agent gets at least one worker from the shuffled list.
         The remaining workers are distributed randomly.
         The random.shuffle(workers) ensures that the initial distribution
         of workers to agents is random.
         The random.choice(agent_ids) assigns the remaining workers in a random way,
-        ensuring no agent is left out
+        ensuring no agent is left out.
         """
 
         # make a copy of the workers list
@@ -82,14 +97,14 @@ class RandomDeploymentPolicy(DeploymentPolicy):
 
         # start by assigning one worker to each agent randomly
         random.shuffle(workers)  # shuffle workers to ensure randomness
-        distribution = {agent_id: [workers.pop()["id"]] for agent_id in agent_ids}
+        distribution = {agent_id: [workers.pop().id] for agent_id in agent_ids}
 
         # distribute the remaining workers randomly
         while workers:
             agent_id = random.choice(agent_ids)  # choose an agent randomly
-            distribution[agent_id].append(workers.pop()["id"])
+            distribution[agent_id].append(workers.pop().id)
 
-        return distribution
+        return self._get_agent_updated_cfg(distribution, job_config)
 
 
 class DeploymentPolicyFactory:
